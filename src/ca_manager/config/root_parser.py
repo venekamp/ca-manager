6a9f098cadda7a0e4cff143440ab20877982e87d
Gerben Venekamp <gerben@venekamp.eu>
2026-01-02 09:47:44 +0100

    Use type over TypeAlias

--- src/ca_manager/config/root_parser.py
+++ src/ca_manager/config/root_parser.py
@@ -1,16 +1,17 @@
 from __future__ import annotations
 
 from collections.abc import Callable
-from typing import TypeAlias, cast
+from typing import cast
 
 from ca_manager.settings import Settings, SettingsKwargs
+
 from .section_spec import RootSectionSpec
 from .specs import ROOT_LEAF_PARSERS, ROOT_SECTION_SPECS
 
-LeafDefaultFactory: TypeAlias = Callable[[], object]
-LeafValueParser: TypeAlias = Callable[[object, str], object]
+type LeafDefaultFactory = Callable[[], object]
+type LeafValueParser = Callable[[object, str], object]
 
-LeafSpec: TypeAlias = tuple[LeafDefaultFactory, LeafValueParser]
+type LeafSpec = tuple[LeafDefaultFactory, LeafValueParser]
 
 
 def parse_settings_root(raw_yaml: dict[str, object]) -> Settings:
